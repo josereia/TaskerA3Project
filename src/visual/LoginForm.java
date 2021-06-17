705420a44fe5192236219489d2e5f4db244cfdca
joaol <joaol@DESKTOP-948E71D>
2021-06-17 15:49:16 -0300

    Add login window

---
+++ src/visual/LoginForm.java
@@ -0,0 +1,44 @@
+package visual;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class LoginForm {
+
+	private JFrame frmLogin;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					LoginForm window = new LoginForm();
+					window.frmLogin.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public LoginForm() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frmLogin = new JFrame();
+		frmLogin.setTitle("Login");
+		frmLogin.setBounds(100, 100, 450, 300);
+		frmLogin.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
