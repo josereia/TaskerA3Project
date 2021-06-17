f4964795623ebd65e0e0747013dcefb83b6de690
joaol <joaol@DESKTOP-948E71D>
2021-06-17 18:34:41 -0300

    Add register form

--- src/visual/LoginForm.java
+++ src/visual/LoginForm.java
@@ -3,10 +3,22 @@ package visual;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JButton;
+import java.awt.BorderLayout;
+import javax.swing.JPanel;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import java.awt.Toolkit;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
 public class LoginForm {
 
 	private JFrame frmLogin;
+	private JTextField textField;
+	private JTextField textField_1;
 
 	/**
 	 * Launch the application.
@@ -15,6 +27,8 @@ public class LoginForm {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
+					//define o estilo da janela para o estilo padrão do sistema operacional
+					javax.swing.UIManager.setLookAndFeel(javax.swing.UIManager.getSystemLookAndFeelClassName());
 					LoginForm window = new LoginForm();
 					window.frmLogin.setVisible(true);
 				} catch (Exception e) {
@@ -36,9 +50,58 @@ public class LoginForm {
 	 */
 	private void initialize() {
 		frmLogin = new JFrame();
+		frmLogin.setIconImage(Toolkit.getDefaultToolkit().getImage("assets/logo.png"));
+		frmLogin.setResizable(false);
 		frmLogin.setTitle("Login");
-		frmLogin.setBounds(100, 100, 450, 300);
-		frmLogin.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	}
+		frmLogin.setBounds(100, 100, 240, 240);
+		//faz com que a janela inicie no centro da tela
+		frmLogin.setLocationRelativeTo(null);
 
+		
+		JPanel panel = new JPanel();
+		frmLogin.getContentPane().add(panel, BorderLayout.SOUTH);
+		
+		JButton btn_login = new JButton("Entrar");
+		btn_login.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+			}
+		});
+		
+		panel.add(btn_login);
+		
+		JPanel panel_1 = new JPanel();
+		frmLogin.getContentPane().add(panel_1, BorderLayout.CENTER);
+		panel_1.setLayout(null);
+		
+		JLabel lblNewLabel_1 = new JLabel("Login:");
+		lblNewLabel_1.setBounds(44, 21, 46, 14);
+		panel_1.add(lblNewLabel_1);
+		
+		textField = new JTextField();
+		textField.addKeyListener(new KeyAdapter() {
+			@Override
+			public void keyPressed(KeyEvent e) {
+				if(e.getKeyCode() == 112) {
+					new RegisterForm().setVisible(true);
+				}
+			}
+		});
+		textField.setBounds(44, 42, 146, 20);
+		panel_1.add(textField);
+		textField.setColumns(10);
+		
+		JLabel lblNewLabel_1_1 = new JLabel("Senha:");
+		lblNewLabel_1_1.setBounds(44, 73, 46, 14);
+		panel_1.add(lblNewLabel_1_1);
+		
+		textField_1 = new JTextField();
+		textField_1.setColumns(10);
+		textField_1.setBounds(44, 94, 146, 20);
+		panel_1.add(textField_1);
+		
+		JLabel lblNewLabel = new JLabel("Esqueceu a senha?");
+		lblNewLabel.setBounds(44, 135, 140, 14);
+		panel_1.add(lblNewLabel);
+	}
 }
