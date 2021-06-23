dda1e019ac748015e06e25e8e6b4f10191374f2b
profe <profe@DESKTOP-5RDRCSI>
2021-06-22 21:34:35 -0300

    teste de commit

--- src/connection/ConnectionFactory.java
+++ src/connection/ConnectionFactory.java
@@ -13,7 +13,7 @@ public class ConnectionFactory {
 	private static final String URL = "jdbc:mysql://127.0.0.1:3306/tasker";
 	private static final String USER = "root";
 	private static final String PASSWORD = "";
-
+	//Christopher
 	public static Connection getConnection() {
 		Connection conn = null;
 		try {
